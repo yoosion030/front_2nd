8da49e9bb61567c7fa46bb8f1777429b3211f2d8
yoosion030 <yoosioff@gmail.com>
2024-06-15 20:59:43 +0900

    1주차 과제 제출
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { Cat, Dog } from "./PureComponentTest.components.tsx";
 
+const MemoizedCat = memo(Cat);
+const MemoizedDog = memo(Dog);
 
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
@@ -9,10 +11,14 @@ export default function PureComponentTest() {
 
   return (
     <div>
-      <Cat crying={meowCount}/>
-      <Dog crying={barkedCount}/>
-      <button data-testid="meow" onClick={() => setMeowCount(n => n + 1)}>야옹</button>
-      <button data-testid="bark" onClick={() => setBarkedCount(n => n + 1)}>멍멍</button>
+      <MemoizedCat crying={meowCount} />
+      <MemoizedDog crying={barkedCount} />
+      <button data-testid="meow" onClick={() => setMeowCount((n) => n + 1)}>
+        야옹
+      </button>
+      <button data-testid="bark" onClick={() => setBarkedCount((n) => n + 1)}>
+        멍멍
+      </button>
     </div>
   );
 }
