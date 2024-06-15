7eb591aeb419cb041f812c608141f52d937eee8e
yoosion030 <yoosioff@gmail.com>
2024-06-15 20:59:43 +0900

    1주차 과제 제출
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-1/src/basic/UseStateTest.tsx
+++ packages/assignment-1/src/basic/UseStateTest.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
-
 // NOTE: state의 값이 정상적으로 변경이 되도록 만들어주세요.
 export default function UseStateTest() {
   const [state, setState] = useState({ bar: { count: 1 } });
 
   const increment = () => {
-    state.bar.count += 1;
-    setState(state);
-  }
+    setState((prev) => ({ ...prev, bar: { count: prev.bar.count + 1 } }));
+  };
 
   return (
     <div>
