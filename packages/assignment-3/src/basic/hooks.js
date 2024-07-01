121b653ff3f1f001637d045cacc230780292f9a3
yoosion030 <yoosioff@gmail.com>
2024-07-02 00:22:01 +0900

    3주차 과제 풀이 - hooks
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,15 +1,46 @@
-export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
-  };
+export const createHooks = (callback) => {
+  let index = 0;
+  const state = [];
 
-  const useMemo = (fn, refs) => {
-    return fn();
+  const useState = (initState) => {
+    if (state.length === index) {
+      state.push(initState);
+    }
+
+    const currentIndex = index;
+    const currentState = state[currentIndex];
+
+    const setState = (() => {
+      let currentIndex = index;
+      return (value) => {
+        if (state[currentIndex] === value) return;
+
+        state[currentIndex] = value;
+        callback();
+      };
+    })();
+
+    index += 1;
+    return [currentState, setState];
   };
 
   const resetContext = () => {
+    index = 0;
+  };
 
-  }
+  const useMemo = (() => {
+    let cache = {};
+    let ref = [];
+    return (fn, refs) => {
+      if (refs.every((v, k) => v === ref[k])) {
+        return cache[refs];
+      } else {
+        ref = refs;
+        cache[refs] = fn;
+        return cache[refs];
+      }
+    };
+  })();
 
   return { useState, useMemo, resetContext };
-}
+};
