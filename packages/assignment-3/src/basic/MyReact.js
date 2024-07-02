67e4a047d2450dccba67ec42f5ccdb4a876f70af
yoosion030 <yoosioff@gmail.com>
2024-07-03 00:37:24 +0900

    3주차 과제 풀이 - MyReact
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -2,10 +2,33 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let currentRootNode;
+  let currentComponentNode;
+  let oldNode;
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  const _render = () => {
+    if (currentComponentNode && currentRootNode) {
+      resetHookContext();
+      const newNode = currentComponentNode();
+      updateElement(currentRootNode, newNode, oldNode);
+      oldNode = newNode;
+    }
+  };
+  function render($root, rootComponent) {
+    currentRootNode = $root;
+    currentComponentNode = rootComponent;
+    resetHookContext();
+
+    const newNode = rootComponent();
+    updateElement(currentRootNode, newNode);
+    oldNode = newNode;
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
