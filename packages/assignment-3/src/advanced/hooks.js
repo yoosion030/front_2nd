ffc7d9d913527c1533b4174395fe6ac01e65ed47
yoosion030 <yoosioff@gmail.com>
2024-07-03 23:50:37 +0900

    3주차 과제 풀이 - advanced hooks
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,6 +1,27 @@
 export function createHooks(callback) {
+  let index = 0;
+  const state = [];
+  let nextFrameCallback;
+
   const useState = (initState) => {
-    return [];
+    if (state.length === index) {
+      state.push(initState);
+    }
+
+    const currentIndex = index;
+    const currentState = state[currentIndex];
+
+    index += 1;
+
+    const setState = (value) => {
+      if (state[currentIndex] === value) return;
+
+      state[currentIndex] = value;
+      cancelAnimationFrame(nextFrameCallback);
+      nextFrameCallback = requestAnimationFrame(callback);
+    };
+
+    return [currentState, setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +29,8 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    index = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
