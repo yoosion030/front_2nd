7b7b7b278bba2701dc1d78f8354e3bb7a39fe67a
yoosion030 <yoosioff@gmail.com>
2024-07-12 22:41:08 +0900

    4주차 과제 풀이 - basic 리팩토링, 테스트 코드 추가
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-4/src/basic/main.basic.js
+++ packages/assignment-4/src/basic/main.basic.js
@@ -1,10 +1,6 @@
 import { CartItem } from "./CartItem";
-import {
-  appendChildElement,
-  appendSelectOptions,
-  createDOMElement,
-  setAttribute,
-} from "./render";
+import { appendChildElement, createDOMElement, setAttribute } from "./render";
+import renderCart from "./renderCart";
 import {
   calculateDiscount,
   calculateDiscountRate,
@@ -20,68 +16,8 @@ const PRODUCTS = [
 ];
 
 const main = () => {
-  const app = document.getElementById("app");
-  const container = createDOMElement({
-    name: "div",
-    options: { className: "bg-gray-100 p-8" },
-  });
-  appendChildElement({ parent: app, children: [container] });
-
-  const cartContainer = createDOMElement({
-    name: "div",
-    options: {
-      className:
-        "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8",
-    },
-  });
-  appendChildElement({ parent: container, children: [cartContainer] });
-
-  const title = createDOMElement({
-    name: "h1",
-    options: {
-      className: "text-2xl font-bold mb-4",
-      textContent: "장바구니",
-    },
-  });
-  const cartItemsContainer = createDOMElement({
-    name: "div",
-    options: { id: "cart-items" },
-  });
-  const cartTotalContainer = createDOMElement({
-    name: "div",
-    options: {
-      id: "cart-total",
-      className: "text-xl font-bold my-4",
-    },
-  });
-  const addButton = createDOMElement({
-    name: "button",
-    options: {
-      id: "add-to-cart",
-      className: "bg-blue-500 text-white px-4 py-2 rounded",
-      textContent: "추가",
-    },
-  });
-  const productSelect = createDOMElement({
-    name: "select",
-    options: {
-      id: "product-select",
-      className: "border rounded p-2 mr-2",
-    },
-  });
-
-  appendSelectOptions({ select: productSelect, options: PRODUCTS });
-
-  appendChildElement({
-    parent: cartContainer,
-    children: [
-      title,
-      productSelect,
-      addButton,
-      cartItemsContainer,
-      cartTotalContainer,
-    ],
-  });
+  const { cartTotalContainer, cartItemsContainer, productSelect, addButton } =
+    renderCart(PRODUCTS);
 
   const updateCart = () => {
     let originalTotal = 0;
