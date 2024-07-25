50211ce94bf4b6a5426b72bc2703067df7fac776
yoosion030 <yoosioff@gmail.com>
2024-07-25 20:41:45 +0900

    fix: comment 리뷰어와 assign 리뷰어가 다른 이슈 해결
    reviewer를 함수 실행할 때 주입하도록 해결
    
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- .github/scripts/review.js
+++ .github/scripts/review.js
@@ -1,35 +1,49 @@
-const reviewers = ["yoosion030", "dhp94d", "lydiahjchung", "callipenguin"];
-
-const getRandomReviewer = (reviewers) => {
+const getRandomReviewer = () => {
+  const reviewers = ["yoosion030", "dhp94d", "lydiahjchung", "callipenguin"];
   const randomReviewer =
     reviewers[Math.floor(Math.random() * reviewers.length)];
 
   return randomReviewer;
 };
 
-const commentReviewer = async ({ github, context }) => {
-  const randomReviewer = getRandomReviewer(reviewers);
-
+const commentReviewer = async ({ github, context, reviewer }) => {
   await github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: `@${randomReviewer}, 리뷰를 부탁드립니다.`,
+    body: `@${reviewer}, 리뷰를 부탁드립니다.`,
   });
 };
 
-const assignReviewer = async ({ github, context }) => {
-  const randomReviewer = getRandomReviewer(reviewers);
+const assignReviewer = async ({ github, context, reviewer }) => {
+  const currentReviewers = await github.rest.pulls.listRequestedReviewers({
+    pull_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  if (currentReviewers.data.users.length > 0) {
+    const reviewersToRemove = currentReviewers.data.users.map(
+      (user) => user.login
+    );
+    await github.rest.pulls.removeRequestedReviewers({
+      pull_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      reviewers: reviewersToRemove,
+    });
+  }
 
   await github.rest.pulls.requestReviewers({
     pull_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    reviewers: [randomReviewer],
+    reviewers: [reviewer],
   });
 };
 
 module.exports = {
   commentReviewer,
   assignReviewer,
+  getRandomReviewer,
 };
